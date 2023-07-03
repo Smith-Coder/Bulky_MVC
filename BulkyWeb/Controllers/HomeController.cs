37b833871f60dccaa5368a1c9c24a88acc87fc5f
Smith C D <Smith.CD@trivium-esolutions.com>
2023-07-03 11:07:12 +0530

    Update

--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace BulkyWeb.Controllers
         public IActionResult Index()
         {
             return View();
+            // return View("Privacy");
         }
 
         public IActionResult Privacy()
---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //passing options to base class that is DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        // to override the default behaviour
+        //function available in DbContext
+        //this function is a default thing that entity framework has implemented and we are just using that to seed data
+        // hasData is used to add data
+        //now we are telling entity framework core that hey, on category, we want to create these three record
+        //now add migration
+        //update database command
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
